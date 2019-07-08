7b282344b1406d5f58eb4bf2be7e4076a9cf7c1c
Romanova El <romanova.bim@gmail.com>
2019-07-08 11:37:59 +0500

    2 window

---
+++ src/Controller.java
@@ -0,0 +1,73 @@
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class Controller {
+    @FXML
+    Button btn1;
+
+    @FXML
+    private Button btn2;
+
+    @FXML
+    private Button btn3;
+
+    public void onExit(ActionEvent actionEvent) {
+        Platform.exit();
+    }
+
+    public void initialize(){
+        btn1.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                try{
+                    Stage stage = new Stage ();
+                    Parent root = FXMLLoader.load(getClass().getResource("1.fxml"));
+                    stage.setTitle ("ОКНО 1. ОПИСАНИЕ ЗАДАЧИ");
+                    stage.setMinHeight (330);
+                    stage.setMinWidth (600);
+                    stage.setResizable (true);
+                    stage.setScene (new Scene(root));
+                    stage.initModality (Modality.NONE);
+                    stage.show ();
+                }catch (Exception ex){
+                    ex.printStackTrace ();
+                }
+            }
+        });
+
+        btn2.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                try{
+                    Stage stage = new Stage ();
+                    Parent root = FXMLLoader.load(getClass().getResource("2.fxml"));
+                    stage.setTitle ("ОКНО 2. РЕШИТЬ ЗАДАЧУ");
+                    stage.setMinHeight (330);
+                    stage.setMinWidth (600);
+                    stage.setResizable (true);
+                    stage.setScene (new Scene(root));
+                    stage.initModality (Modality.NONE);
+                    stage.show ();
+                }catch (Exception ex){
+                    ex.printStackTrace ();
+                }
+            }
+        });
+    }
+
+
+
+}
---
+++ src/Controller2.java
@@ -0,0 +1,31 @@
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
+public class Controller2 {
+
+    @FXML
+    TextField aX;
+
+    @FXML
+    TextField aY;
+
+    @FXML
+    TextField bX;
+
+    @FXML
+    TextField bY;
+
+    @FXML
+    TextField cX;
+
+    @FXML
+    TextField cY;
+
+    @FXML
+    TextField r;
+
+    public void init(){
+
+    }
+}
