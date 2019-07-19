12866b69bcd519b3f73baa373a1a0d1a42a55c4d
Romanova El <romanova.bim@gmail.com>
2019-07-19 16:38:51 +0500

    2

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main extends Application{
 
    public void start(Stage primaryStage) throws Exception{
        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-       primaryStage.setTitle("Задание 07014.Елена Романова");
+       primaryStage.setTitle("Задание 07014. Елена Романова");
        primaryStage.setScene(new Scene(root, 600, 330));
        primaryStage.show();
    }
